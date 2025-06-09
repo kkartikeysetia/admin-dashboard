cdf2b2d6d7bd0680978455dd47caac3d7cfd7db5
kkartikeysetia <143159958+kkartikeysetia@users.noreply.github.com>
2025-06-09 20:44:21 +0530

    minor bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ category }) => (
   <div className="mb-10">
     <p className="text-lg text-gray-400">{category}</p>
     <p className="text-3xl font-extrabold tracking-tight text-slate-900">
-      Admin Dashboard
+      Reactify Admin
     </p>
   </div>
 );
